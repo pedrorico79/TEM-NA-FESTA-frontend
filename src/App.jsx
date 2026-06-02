28146ac25d988ef32a238afac79496177c819645
LauraBuzzato <laura.buzzato@sptech.school>
2026-06-02 14:40:53 -0300

    adicionando navegacao entre paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,59 @@
-import Menu from "./components/shared/menu/Menu";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from "./components/pages/Login";
+import NovoPedido from "./components/pages/NovoPedido";
+import PaginaInicial from "./components/pages/PaginaInicial";
+import Pedidos from "./components/pages/Pedidos";
+import Produtos from "./components/pages/Produtos";
+import Relatorios from "./components/pages/Relatorios";
 
 function App() {
-  return <Menu />;
+
+  return (
+
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route
+          path="/"
+          element={<Login />}
+        />
+
+        <Route
+          path="/PaginaInicial"
+          element={<PaginaInicial />}
+        />
+
+
+        <Route
+          path="/Pedidos"
+          element={<Pedidos />}
+        />
+
+        <Route
+          path="/Produtos"
+          element={<Produtos />}
+        />
+
+        <Route
+          path="/NovoPedido"
+          element={<NovoPedido />}
+        />
+        
+        <Route
+          path="/Relatorios"
+          element={<Relatorios />}
+        />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
 }
 
 export default App;
