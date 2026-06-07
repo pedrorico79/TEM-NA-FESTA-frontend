6a39aadf0fecdea1bc12a42f580f85b975565251
LauraBuzzato <laura.buzzato@sptech.school>
2026-06-06 22:28:12 -0300

    Adicionando navegacao entre paginas

--- src/App.jsx
+++ src/App.jsx
@@ -1,59 +1,8 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-
-import Login from "./components/pages/Login";
-import NovoPedido from "./components/pages/NovoPedido";
-import PaginaInicial from "./components/pages/PaginaInicial";
-import Pedidos from "./components/pages/Pedidos";
-import Produtos from "./components/pages/Produtos";
-import Relatorios from "./components/pages/Relatorios";
+import { RouterProvider } from "react-router-dom";
+import { routes } from "./routes";
 
 function App() {
-
-  return (
-
-    <BrowserRouter>
-
-      <Routes>
-
-        <Route
-          path="/"
-          element={<Login />}
-        />
-
-        <Route
-          path="/PaginaInicial"
-          element={<PaginaInicial />}
-        />
-
-
-        <Route
-          path="/Pedidos"
-          element={<Pedidos />}
-        />
-
-        <Route
-          path="/Produtos"
-          element={<Produtos />}
-        />
-
-        <Route
-          path="/NovoPedido"
-          element={<NovoPedido />}
-        />
-        
-        <Route
-          path="/Relatorios"
-          element={<Relatorios />}
-        />
-
-      </Routes>
-
-    </BrowserRouter>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Login from "./components/pages/Login";
+import NovoPedido from "./components/pages/NovoPedido";
+import PaginaInicial from "./components/pages/PaginaInicial";
+import Pedidos from "./components/pages/Pedidos";
+import Produtos from "./components/pages/Produtos";
+import Relatorios from "./components/pages/Relatorios";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+    errorElement: <div>Erro</div>,
+  },
+  {
+    path: "/PaginaInicial",
+    element: <PaginaInicial />,
+  },
+  {
+    path: "/Pedidos",
+    element: <Pedidos />,
+  },
+  {
+    path: "/Produtos",
+    element: <Produtos />,
+  },
+  {
+    path: "/NovoPedido",
+    element: <NovoPedido />,
+  },
+  {
+    path: "/Relatorios",
+    element: <Relatorios />,
+  },
+]);
